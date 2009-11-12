5075e17cd3341f1e045c95d4801f903841ac1226
Vitaly Kushner <vitaly@astrails.com>
2009-11-12 12:38:16 +0200

    vladify

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+# require "vladify/delayed_job"
+# require "vladify/delayed_job_monit"
+# require "vladify/fast_gettext"
+# require "vladify/gettext"
+# require "vladify/thinking_sphinx"
+# require "vladify/ultrasphinx"
+# require "vladify/whenever"
+# require "vladify/workling"
+
+set :application, "benyehuda.com"
+set :repository, "git@astrails.com:benyehuda"
+set :domain, "astrails@astrails.com"
