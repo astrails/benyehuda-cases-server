61ec89ca4c52e46c118b90cfed3d1e2feae475bb
Vitaly Kushner <vitaly@astrails.com>
2009-11-12 12:37:41 +0200

    rspec

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'rspec-rails', :lib => false, :version => '1.2.6'
+  config.gem 'rspec', :lib => false, :version => '1.2.6'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
