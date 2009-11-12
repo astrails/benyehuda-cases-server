a4af7312628cf4565526450c297f4958a169728e
Vitaly Kushner <vitaly@astrails.com>
2009-11-12 12:38:18 +0200

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => :pages, :action => :show, :id => :home
+
   map.resources :pages, :controller => 'pages', :only => [:show]
 
 end
