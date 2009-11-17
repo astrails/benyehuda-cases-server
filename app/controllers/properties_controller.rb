95dfce4521a541626c577d1b1b949be182cd597a
Boris Nadion <boris@astrails.com>
2009-11-17 19:59:06 +0200

    object properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -10,6 +10,23 @@ class PropertiesController < InheritedResources::Base
     end
   end
 
+  def update
+    super do |format|
+      format.html {redirect_to properties_path}
+    end
+  end
+
+  def destroy
+    super do |format|
+      format.js do
+        flash.delete :notice
+        render(:update) do |page|
+          page << "jQuery('##{dom_id(@property)}').remove();"
+        end
+      end
+    end
+  end
+
 protected
   def collection
     @properties ||= end_of_association_chain.by_parent_type.by_title.paginate(:page => params[:page], :per_page => params[:per_page])
