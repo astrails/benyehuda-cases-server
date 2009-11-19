aadb0aad74f2dc476183bf29ac2e041757537ed0
Boris Nadion <boris@astrails.com>
2009-11-19 11:36:16 +0200

    unactivated users edit fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < InheritedResources::Base
 
   def update
     @user = User.find(params[:id])
+    unless @user.activated_at
+      flash[:error] = "You cannot edit unactivated users"
+      redirect_to users_path
+      return
+    end
     # manual update protected attributes
     if current_user.is_admin?
       @user.is_admin = params[:user].delete(:is_admin)
