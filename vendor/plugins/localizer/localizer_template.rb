ead78d3bc12ac3ad7e39ffcef52e81d2ce99211a
Vitaly Kushner <vitaly@astrails.com>
2009-12-24 01:21:18 +0200

    gem spec

--- vendor/plugins/localizer/localizer_template.rb
+++ vendor/plugins/localizer/localizer_template.rb
@@ -2,3 +2,5 @@ gem "fast_gettext", :source => "http://gemcutter.org/"
 gem "gettext", :source => "http://gemcutter.org/", :lib => false
 gem "ruby_parser", :source => "http://gemcutter.org/", :lib => false
 text_field_tag
+
+./script/runner "puts Gem.loaded_specs['fast_gettext'].to_yaml" > vendor/gems/fast_gettext-0.4.17/.specification
