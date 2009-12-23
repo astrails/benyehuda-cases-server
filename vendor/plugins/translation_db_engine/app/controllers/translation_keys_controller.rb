8156d10912f1fd97ec0c198cc54cb4af7e0df307
Vitaly Kushner <vitaly@astrails.com>
2009-12-23 21:03:37 +0200

    Braid: Added mirror 'vendor/plugins/translation_db_engine' at 'a12af29'

---
+++ vendor/plugins/translation_db_engine/app/controllers/translation_keys_controller.rb
@@ -0,0 +1,83 @@
+class TranslationKeysController < ActionController::Base
+  before_filter :authenticate
+  before_filter :find_translation_key, :only=>%w[show edit update destroy]
+
+  #use host layout/helpers
+  helper :all
+  layout :choose_layout
+
+  #prevent 'method missing' for normally controller-side helpers
+  def current_user;nil;end
+  def logged_in?;false;end
+  helper_method :current_user, :logged_in?
+
+  def index
+    @translation_keys = TranslationKey.find(:all)
+  end
+
+  def new
+    @translation_key = TranslationKey.new
+    add_default_locales_to_translation
+    render :action=>:edit
+  end
+
+  def create
+    @translation_key = TranslationKey.new(params[:translation_key])
+    if @translation_key.save
+      flash[:notice] = 'Created!'
+      redirect_to translation_key_path(@translation_key)
+    else
+      flash[:error] = 'Failed to save!'
+      render :action=>:edit
+    end
+  end
+
+  def show
+    render :action=>:edit
+  end
+
+  def edit
+  end
+
+  def update
+    if @translation_key.update_attributes(params[:translation_key])
+      flash[:notice] = 'Saved!'
+      redirect_to @translation_key
+    else
+      flash[:error] = 'Failed to save!'
+      render :action=>:edit
+    end
+  end
+
+  def destroy
+    @translation_key.destroy
+    redirect_to translation_keys_path
+  end
+
+  protected
+
+  def self.config
+    @@config ||= YAML::load(File.read(Rails.root.join('config','translation_db_engine.yml'))).with_indifferent_access rescue {}
+  end
+
+  def choose_layout
+    @@config[:layout] || 'application'
+  end
+
+  def authenticate
+    return unless auth = self.class.config[:auth]
+    authenticate_or_request_with_http_basic do |username, password|
+      username == auth[:name] && password == auth[:password]
+    end
+  end
+
+  def find_translation_key
+    @translation_key = TranslationKey.find(params[:id])
+  end
+
+  def add_default_locales_to_translation
+    TranslationKey.available_locales.each do |locale|
+      @translation_key.translations.build(:locale=>locale)
+    end
+  end
+end
