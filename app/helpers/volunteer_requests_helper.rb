94d7ef828b9d33a2dcccd149b91422f64f7d9702
Boris Nadion <boris@astrails.com>
2009-12-25 16:26:31 +0200

    gettextify views

--- app/helpers/volunteer_requests_helper.rb
+++ app/helpers/volunteer_requests_helper.rb
@@ -1,5 +1,5 @@
 module VolunteerRequestsHelper
   def confirm_button(volunteer_request)
-    link_to _("Confirm"), volunteer_request_path(volunteer_request), :method => :put, :confirm => "Are you sure?"
+    link_to _("Confirm"), volunteer_request_path(volunteer_request), :method => :put, :confirm => _('Are you sure?')
   end
 end
