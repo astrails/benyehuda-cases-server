41804c52ba6ccfc455e363260c122ce673553bd4
Boris Nadion <boris@astrails.com>
2010-02-11 20:04:05 +0200

    volunteer requests in dashboard

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,10 @@
 module DashboardsHelper
+  def pending_volunteer_requests
+    pending_count = VolunteerRequest.pending.count
+    if pending_count > 0
+      haml_tag(:h5) do
+        haml_concat link_to((n_('There is %d pending volunter request', 'There are %d pending volunter requests', pending_count) % pending_count), volunteer_requests_path)
+      end
+    end
+  end
 end
