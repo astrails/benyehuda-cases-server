270b4d14dbe41d7788244294a09b27e3f548f659
Boris Nadion <boris@astrails.com>
2010-02-26 11:11:23 +0200

    css class for finished comment

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,6 +4,7 @@ module CommentsHelper
       res << "editor_eyes_only" if comment.editor_eyes_only?
       res << "rejection_reason" if comment.is_rejection_reason?
       res << "abandoning_reason" if comment.is_abandoning_reason?
+      res << "finished_reason" if comment.is_finished_reason?
     end.join(" ")
   end
 end
