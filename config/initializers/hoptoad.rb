862e46d4cf67a47129d610fd30ba7788cbb4a22c
Boris Nadion <boris@astrails.com>
2010-06-16 11:04:46 +0300

    proxy for hoptoad notifier

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,8 @@
 HoptoadNotifier.configure do |config|
   config.api_key = 'b5829a39945a374e55edda26ca92d34a'
   config.environment_filters << 'rack-bug.*'
+  if "benyehuda" == `hostname`.strip
+    config.proxy_host = 'proxy1.haifa.ac.il'
+    config.proxy_port = 8080
+  end
 end
