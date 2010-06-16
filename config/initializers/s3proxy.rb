267f1c6457697868c7d973f0ee531a5f32a9c2ef
Boris Nadion <boris@astrails.com>
2010-06-16 11:42:39 +0300

    oops

--- config/initializers/s3proxy.rb
+++ config/initializers/s3proxy.rb
@@ -3,7 +3,7 @@ module AWS
     class Connection
       class << self
         def connect_with_proxy(options = {})
-          connect_without_proxy(options.merge(:proxy => {:host => PROXY_HOST, :port => PROXY_PORT})
+          connect_without_proxy(options.merge(:proxy => {:host => PROXY_HOST, :port => PROXY_PORT}))
         end
         alias_method_chain(:connect, :proxy) if defined?(PROXY_HOST) && defined?(PROXY_PORT)
       end
