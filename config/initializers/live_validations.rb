894d1b185b199bbe0a9b017d018718773ebb21f5
Daniel Lurie <daniel@astrails.com>
2012-03-13 16:48:24 +0200

    removed live validation initializer

--- config/initializers/live_validations.rb
+++
@@ -1,6 +0,0 @@
-LiveValidations.use :jquery_validations, :default_valid_message => "", :validate_on_blur => true, 
-  :validator_settings => {
-    :errorElement => "p", :errorClass => "inline-error",
-    :highlight => "function(element, errorClass){jQuery(element).parent('li').addClass('error')}",
-    :unhighlight => "function(element, errorClass){jQuery(element).parent('li').removeClass('error')}"
-  }
