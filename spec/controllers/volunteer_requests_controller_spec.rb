65a66a864c7712aaf6db2b41091562c5afa2aa6e
Daniel Lurie <daniel@astrails.com>
2012-03-21 17:46:17 +0200

    more rails 3 format changes

--- spec/controllers/volunteer_requests_controller_spec.rb
+++ spec/controllers/volunteer_requests_controller_spec.rb
@@ -41,10 +41,12 @@ describe VolunteerRequestsController do
 
     it "should approve" do
       @volunteer_request.approved_at.should be_blank
-      Notification.should_receive(:deliver_volnteer_welcome)
+      ActionMailer::Base.deliveries = []
+      ActionMailer::Base.deliveries.count.should == 0
       put :update, :id => @volunteer_request.id
       response.should redirect_to(volunteer_requests_path)
       @volunteer_request.reload.approved_at.should_not be_blank
+      ActionMailer::Base.deliveries.count.should == 1
     end
 
     it "should not reapprove" do
