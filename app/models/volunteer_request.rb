bd9b8426fffd82935e534e591311af6ff64e8fb9
Daniel Lurie <daniel@astrails.com>
2012-03-21 13:32:55 +0200

    fix specs and errors format

--- app/models/volunteer_request.rb
+++ app/models/volunteer_request.rb
@@ -2,18 +2,17 @@ class VolunteerRequest < ActiveRecord::Base
   belongs_to :user
   belongs_to :approver, :class_name => "User"
 
-  validates_presence_of :user_id
-  validates_length_of :preferences, :within => 8..4096
+  validates :user_id, :presence => true
+  validates :preferences, :length => { :within => 8..4096 }
 
   attr_accessible :preferences
 
-  named_scope :pending, :conditions => "volunteer_requests.approved_at is NULL"
-  named_scope :by_request_time, :order => "volunteer_requests.created_at"
-  named_scope :with_user, :include => :user
+  scope :pending, where("volunteer_requests.approved_at is NULL")
+  scope :by_request_time, order("volunteer_requests.created_at")
+  scope :with_user, includes(:user)
 
   include CustomProperties
   has_many_custom_properties :request # task_properties
-
 
   def approve!(approver_user)
     self.approver = approver_user
