1630b085b0818a785f92e5a7452b742fcba49e3d
Daniel Lurie <daniel@astrails.com>
2012-03-27 18:18:21 +0200

    task_kind model tests

--- spec/factories/task_kinds.rb
+++ spec/factories/task_kinds.rb
@@ -1,5 +1,3 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :task_kind do |k|
     k.sequence(:name) {|n| "some_kind_#{n}"}
--- spec/models/task_kind_spec.rb
+++ spec/models/task_kind_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe TaskKind do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:task_kind){Factory.create(:task_kind)}
+  let(:task){Factory.create(:task, :task_kind_id => task_kind)}
+
+  it "should create TaskKind" do
+    task_kind
+  end
+
+  it "should delete TaskKind" do
+    task_kind.delete
+  end
+
+  describe :validations do
+    it "should validate name presence" do
+      lambda {
+        Factory.create(:task_kind, :name => nil)
+      }.should raise_error ActiveRecord::RecordInvalid
+    end
+
+    it "should validate name uniqueness" do
+      lambda {
+        Factory.create(:task_kind, :name => task_kind.name)
+      }.should raise_error ActiveRecord::RecordInvalid
+    end
+
+    it "should not delete record if there are associated tasks and add error" do
+      kind = task.kind
+      kind.destroy
+      kind.errors.should_not be_blank
+    end
+  end
 end
