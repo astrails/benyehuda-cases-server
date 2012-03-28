103d83abe6f3a2df5a38d539a132eff14f134a78
Daniel Lurie <daniel@astrails.com>
2012-03-28 14:17:58 +0200

    move into routing folder and fix tests of the routing

--- spec/routing/passwords_routing_spec.rb
+++ spec/routing/passwords_routing_spec.rb
@@ -3,31 +3,31 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe PasswordsController do
   describe "route recognition" do
     it "should generate params { :controller => 'passwords', action => 'index' } from GET /passwords" do
-      {:get, "/passwords"}.should_not be_routable
+      { :get => "/passwords" }.should_not be_routable
     end
 
     it "should generate params { :controller => 'passwords', action => 'new' } from GET /passwords/new" do
-      {:get, "/passwords/new"}.should route_to(:controller => "passwords", :action => "new")
+      { :get => "/passwords/new" }.should route_to(:controller => "passwords", :action => "new")
     end
 
     it "should generate params { :controller => 'passwords', action => 'create' } from POST /passwords" do
-      {:post, "/passwords"}.should route_to(:controller => "passwords", :action => "create")
+      { :post => "/passwords" }.should route_to(:controller => "passwords", :action => "create")
     end
 
     it "should generate params { :controller => 'passwords', action => 'show', id => '1' } from GET /passwords/1" do
-      {:get, "/passwords/1"}.should_not be_routable
+      { :get => "/passwords/1" }.should_not be_routable
     end
 
     it "should generate params { :controller => 'passwords', action => 'edit', id => '1' } from GET /passwords/1;edit" do
-      {:get, "/passwords/1/edit"}.should route_to(:controller => "passwords", :action => "edit", :id => "1")
+      { :get => "/passwords/1/edit" }.should route_to(:controller => "passwords", :action => "edit", :id => "1")
     end
 
     it "should generate params { :controller => 'passwords', action => 'update', id => '1' } from PUT /passwords/1" do
-      {:put, "/passwords/1"}.should route_to(:controller => "passwords", :action => "update", :id => "1")
+      { :put => "/passwords/1" }.should route_to(:controller => "passwords", :action => "update", :id => "1")
     end
 
     it "should generate params { :controller => 'passwords', action => 'destroy', id => '1' } from DELETE /passwords/1" do
-      {:delete => "/passwords/1"}.should_not be_routable
+      { :delete => "/passwords/1" }.should_not be_routable
     end
   end
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -4,31 +4,31 @@ describe UsersController do
   describe "route recognition" do
 
     it "should generate params { :controller => 'users', action => 'index' } from GET /users" do
-      {:get, "/users"}.should route_to(:controller => "users", :action => "index")
+      {:get => "/users"}.should route_to(:controller => "users", :action => "index")
     end
 
     it "should generate params { :controller => 'users', action => 'new' } from GET /users/new" do
-      {:get, "/users/new"}.should route_to(:controller => "users", :action => "new")
+      {:get => "/users/new"}.should route_to(:controller => "users", :action => "new")
     end
 
     it "should generate params { :controller => 'users', action => 'create' } from POST /users" do
-      {:post, "/users"}.should route_to(:controller => "users", :action => "create")
+      {:post => "/users"}.should route_to(:controller => "users", :action => "create")
     end
 
     it "should generate params { :controller => 'users', action => 'show', id => '1' } from GET /users/1" do
-      {:get, "/users/1"}.should route_to(:controller => "users", :action => "show", :id => "1")
+      {:get => "/users/1"}.should route_to(:controller => "users", :action => "show", :id => "1")
     end
 
     it "should generate params { :controller => 'users', action => 'edit', id => '1' } from GET /users/1;edit" do
-      {:get, "/users/1/edit"}.should route_to(:controller => "users", :action => "edit", :id => "1")
+      {:get => "/users/1/edit"}.should route_to(:controller => "users", :action => "edit", :id => "1")
     end
 
     it "should generate params { :controller => 'users', action => 'update', id => '1' } from PUT /users/1" do
-      {:put, "/users/1"}.should route_to(:controller => "users", :action => "update", :id => "1")
+      {:put => "/users/1"}.should route_to(:controller => "users", :action => "update", :id => "1")
     end
 
     it "should generate params { :controller => 'users', action => 'destroy', id => '1' } from DELETE /users/1" do
-      {:delete, "/users/1"}.should route_to(:controller => "users", :action => "destroy", :id => "1")
+      {:delete => "/users/1"}.should route_to(:controller => "users", :action => "destroy", :id => "1")
     end
   end
 end
