8e4a06fffd653d67f62a16b0b54aaf926bf30235
Daniel Lurie <daniel@astrails.com>
2012-04-02 18:46:07 +0300

    seed kinds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,3 +8,11 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 GlobalPreference.set!('domain', 'dev.tanin:3000')
+
+VolunteerKind.create(
+  [
+    {:name => "סריקה"},
+    {:name => "עריכה טכנית"},
+    {:name => "רשות פרסום"}
+  ]
+)
