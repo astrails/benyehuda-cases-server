77c76eb24837aa6c748b2bae6858d628245cdb0d
Daniel Lurie <daniel@astrails.com>
2012-04-08 17:38:30 +0300

    create and list volunteer kinds by ajax with popup

--- app/controllers/admin/volunteer_kinds_controller.rb
+++ app/controllers/admin/volunteer_kinds_controller.rb
@@ -1,10 +1,5 @@
 class Admin::VolunteerKindsController < InheritedResources::Base
   before_filter :require_admin
   actions :new, :create, :index, :destroy
-
-  protected
-
-  def collection
-    @volunteer_kinds ||= super.paginate(:page => params[:page], :per_page => 25)
-  end
+  respond_to :js
 end
