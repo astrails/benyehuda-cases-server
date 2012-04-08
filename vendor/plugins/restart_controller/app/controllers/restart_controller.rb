c5efad69ce3b4c1b71615e77eec869025213f6fe
Daniel Lurie <daniel@astrails.com>
2012-04-08 16:28:46 +0300

    removed restart plugin

--- vendor/plugins/restart_controller/app/controllers/restart_controller.rb
+++
@@ -1,8 +0,0 @@
-class RestartController < ApplicationController
-  before_filter :require_admin
-
-  def restart
-    system("touch #{RAILS_ROOT}/tmp/restart.txt")
-    redirect_to "/"
-  end
-end
--- vendor/plugins/restart_controller/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  match "/restart" => "restart#restart", :via => :post
-end
