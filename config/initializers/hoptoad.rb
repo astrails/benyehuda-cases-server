e9179b9fde547162312c04ed92300ff6850a29fb
Costa <costa@astrails.com>
2012-05-04 22:30:12 +0300

    remove hoptoad temporarily (TODO: transfer to airbrake)

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,8 +1,9 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = 'b5829a39945a374e55edda26ca92d34a'
-  # DEPRECATED config.environment_filters << 'rack-bug.*'
-  if defined?(PROXY_HOST) && defined?(PROXY_PORT)
-    config.proxy_host = PROXY_HOST
-    config.proxy_port = PROXY_PORT
-  end
-end
+# TODO replace with airbrake
+#HoptoadNotifier.configure do |config|
+  #config.api_key = 'b5829a39945a374e55edda26ca92d34a'
+  #config.environment_filters << 'rack-bug.*'
+  #if defined?(PROXY_HOST) && defined?(PROXY_PORT)
+    #config.proxy_host = PROXY_HOST
+    #config.proxy_port = PROXY_PORT
+  #end
+#end
