df3fd651b00e05b3acdfe02802d0740dded07843
Daniel Lurie <daniel@astrails.com>
2012-05-07 15:05:37 +0300

    astrails-safe config file

---
+++ config/astrails-safe.rb
@@ -0,0 +1,37 @@
+safe do
+  local :path => "/var/backups/stage.benyehuda.org/:kind/:id"
+ 
+  s3 do
+    key "AKIAJAQH5UIRARAHXIZQ"
+    secret "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"
+    bucket "backup.benyehuda.org"
+    path ":kind/:id"
+  end
+ 
+  keep do
+    local 20
+    s3 100
+  end
+ 
+  mysqldump do
+    options "-ceKq --single-transaction --create-options"
+    user "astrails"
+    password "DM8VEgG4GMIQaN"
+    socket "/var/run/mysqld/mysqld.sock"
+    database :benyehuda_stage
+  end
+
+  gpg do
+    command "/usr/bin/gpg"
+    options  "--no-use-agent"
+    password "Udh567Rvg*I"
+  end
+ 
+  tar do
+    options "-h"
+    archive "stage-benyehuda-org" do
+      files "var/www/staging.benyehuda.org"
+      exclude ["/var/www/staging.benyehuda.org/current/log", "/var/www/staging.benyehuda.org/current/tmp"]
+    end
+  end
+end
