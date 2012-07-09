62170fee314b27f18f9d18353253d40730efc64d
Costa <costa@astrails.com>
2012-07-10 03:49:59 +0900

    upgrade translation text even further

---
+++ db/migrate/20120709182605_upgrade_translation_texts.rb
@@ -0,0 +1,14 @@
+class UpgradeTranslationTexts < ActiveRecord::Migration
+  def self.up
+    say "SORRY it's one-way only!"
+    TranslationText.all.each do |tt|
+      tt.text.gsub(/\{\{([a-z0-9_]*)\}\}/, '%{\\1}')
+      tt.save!
+    end
+  end
+
+  def self.down
+    say "I said SORRY it's one-way only!!!"
+    raise
+  end
+end
